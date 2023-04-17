2c77624d349765b294c10ad57a543b42c488fe11
shaeel45 <beingshaeel@gmail.com>
2023-04-17 14:58:53 +0500

    Voting Code

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var num=(prompt("Enter Your Age For Voting"));
+  if(num<18 || num>60){
+    document.getElementById('sorry').style.display='block'
+    // document.write("Invalid Age")
+  }
+  else if(num>=18){
+    document.getElementById("Form").style.display='block'
+    // document.write("Welcome")
+  }
+  if (num<18) {
+    document.getElementsById('sorry').style.display='block'
+     }  
+
+     var Form = Form
+     if(Form == submit){
+        document.getElementById('congo').style.display='block'
+     }
+     else if(Form != submit){
+        console.log("Nothing To Show")
+     }
